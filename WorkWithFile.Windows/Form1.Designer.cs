57d0eaaa577cf110821ea6dfe2b6be360a453189
nader <nhashemi6@gmail.com>
2023-07-11 11:46:11 +0330

    Add project files.

---
+++ WorkWithFile.Windows/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WorkWithFile.Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSlctFLDR = new System.Windows.Forms.Button();
+            this.btnLstFLDR = new System.Windows.Forms.Button();
+            this.btnMergeFileFldr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnSlctFLDR
+            // 
+            this.btnSlctFLDR.Location = new System.Drawing.Point(484, 29);
+            this.btnSlctFLDR.Name = "btnSlctFLDR";
+            this.btnSlctFLDR.Size = new System.Drawing.Size(105, 23);
+            this.btnSlctFLDR.TabIndex = 1;
+            this.btnSlctFLDR.Text = "select FLDR";
+            this.btnSlctFLDR.UseVisualStyleBackColor = true;
+            this.btnSlctFLDR.Click += new System.EventHandler(this.btnSlctFLDR_Click);
+            // 
+            // btnLstFLDR
+            // 
+            this.btnLstFLDR.Location = new System.Drawing.Point(484, 123);
+            this.btnLstFLDR.Name = "btnLstFLDR";
+            this.btnLstFLDR.Size = new System.Drawing.Size(151, 23);
+            this.btnLstFLDR.TabIndex = 3;
+            this.btnLstFLDR.Text = "save list FLDRs";
+            this.btnLstFLDR.UseVisualStyleBackColor = true;
+            this.btnLstFLDR.Click += new System.EventHandler(this.btnLstFLDR_Click);
+            // 
+            // btnMergeFileFldr
+            // 
+            this.btnMergeFileFldr.Location = new System.Drawing.Point(507, 193);
+            this.btnMergeFileFldr.Name = "btnMergeFileFldr";
+            this.btnMergeFileFldr.Size = new System.Drawing.Size(112, 53);
+            this.btnMergeFileFldr.TabIndex = 4;
+            this.btnMergeFileFldr.Text = "merge file content of fldr";
+            this.btnMergeFileFldr.UseVisualStyleBackColor = true;
+            this.btnMergeFileFldr.Click += new System.EventHandler(this.btnMergeFileFldr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMergeFileFldr);
+            this.Controls.Add(this.btnLstFLDR);
+            this.Controls.Add(this.btnSlctFLDR);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSlctFLDR;
+        private System.Windows.Forms.Button btnLstFLDR;
+        private System.Windows.Forms.Button btnMergeFileFldr;
+    }
+}
+
---
+++ WorkWithFile.Windows/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkWithFile.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnSlctFLDR_Click(object sender, EventArgs e)
+        {
+            var fldrBrwsrDlg = new FolderBrowserDialog();
+            if (fldrBrwsrDlg.ShowDialog() == DialogResult.OK)
+                textBox1.Text = fldrBrwsrDlg.SelectedPath;
+        }
+
+        private void btnLstFLDR_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("Dir !Exist"); return;
+            }
+            var path = Path.GetFullPath(textBox1.Text);
+            var files = Directory.GetDirectories(path);
+            var svFlDlg = new SaveFileDialog();
+
+            if (svFlDlg.ShowDialog() == DialogResult.OK)
+                File.WriteAllLines(svFlDlg.FileName, files, Encoding.UTF8);
+
+            MessageBox.Show("file saved");
+
+        }
+
+        private void btnMergeFileFldr_Click(object sender, EventArgs e)
+        {
+            var path = Path.GetFullPath(textBox1.Text);
+            var files = Directory.GetFiles(path);
+            Encoding encoding = Encoding.UTF8;
+
+            var filename = path + $"\\merge-{DateTime.Now:MM-dd-HH-mm}.txt";
+            File.WriteAllText(filename, "", encoding);
+            foreach (var file in files)
+                File.AppendAllLines(filename, File.ReadAllLines(file, Encoding.Default));
+
+            MessageBox.Show("merge file content of fldr saved!");
+        }
+    }
+}
